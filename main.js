5a9b39f366a1711455077cd9f53660e993ad2324
Rocky Alessandro <144310099+rockhubzz@users.noreply.github.com>
2023-12-17 19:32:35 +0700

    gabut

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("You got mail!")
+let nama= prompt("Nama pertama:")
+let namaa= prompt("Nama kedua: ")
+const angka= [1,2,3,4,5]
+
+alert(`saya adalah ${nama+" "+namaa}`)
+for(let i=angka.length-1; i>=0; i--){
+    alert(angka[i])
+}
